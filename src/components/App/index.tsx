20548e87e28ee522058b181feb1043bdeff1123b
blurbyte <info@blurbyte.com>
2018-04-13 17:51:06 +0200

    Formatted with prettier

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 
 export interface AppProps {
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
 class App extends React.Component<AppProps> {
   render() {
-    return (
-      <div>Hello TypeScript!</div>
-    );
+    return <div>Hello TypeScript!</div>;
   }
 }
 
