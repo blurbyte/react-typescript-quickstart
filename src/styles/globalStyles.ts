f99e5ae91f54663e9ded1c8db983178c34c4d3ce
blurbyte <info@blurbyte.com>
2018-04-15 22:58:07 +0200

    Updated globalStyles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,4 +1,5 @@
 import { injectGlobal } from './styledComponents';
+import { Colors } from './colors';
 
 // tslint:disable-next-line
 injectGlobal`
@@ -9,11 +10,22 @@ injectGlobal`
   }
 
   body {
-    background: #ffffff;
+    background: ${Colors.white};
+    color: ${Colors.gray};
     font-family: 'Open Sans', Helvetica, Arial, sans-serif;
     line-height: 1.4;
     font-size: 1.4rem;
     font-weight: 300;
     overflow-y: scroll;
+  }
+
+  ::-moz-selection {
+    background: rgba(74,108,187,0.2);
+    color: inherit;
+  }
+
+  ::selection {
+    background: rgba(74,108,187,0.2);
+    color: inherit;
   }
 `;
