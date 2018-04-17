b07757c164f97a902a8908d16a41088744601bdd
blurbyte <info@blurbyte.com>
2018-04-17 14:06:10 +0200

    SecondaryHeadline component created

---
+++ src/components/SecondaryHeadline/index.tsx
@@ -0,0 +1,15 @@
+import styled from '@styles/styledComponents';
+import { Colors } from '@styles/colors';
+
+const SecondaryHeadline = styled.h2`
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.4rem;
+  color: ${Colors.darkGray};
+  line-height: 1.2;
+  margin: 1.4rem 0;
+  margin-top: 3rem;
+  padding-top: 1.6rem;
+  border-top: 1px solid ${Colors.gray};
+`;
+
+export default SecondaryHeadline;
