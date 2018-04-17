f96d2f948312fde772ed802007d5888d896cce4d
blurbyte <info@blurbyte.com>
2018-04-17 18:19:28 +0200

    Updated CoreConcepts

--- src/components/CoreConcepts/index.tsx
+++ src/components/CoreConcepts/index.tsx
@@ -17,9 +17,17 @@ class CoreConcepts extends React.Component {
         <TextList>
           <TextList.Item>How to set up TypeScript development environment for React web app</TextList.Item>
           <TextList.Item>How to organize file structure of your project</TextList.Item>
-          <TextList.Item>How to write unit and integration tests</TextList.Item>
           <TextList.Item>
-            How to take advantage of <em>styled-components</em>
+            How to implement and test <strong>render props</strong> component &mdash; <em>src/components/Counter</em>{' '}
+            and <em>src/components/ManageCounter</em>
+          </TextList.Item>
+          <TextList.Item>
+            How to create <strong>compound</strong> component &mdash; <em>src/components/TextList</em>
+          </TextList.Item>
+          <TextList.Item>How to write unit and integration tests</TextList.Item>
+          <TextList.Item>How to setup strightforward CI/CD pipeline with TravisCI and Surge</TextList.Item>
+          <TextList.Item>
+            How to create fully responsive layout with <strong>css grid</strong> and <strong>styled-components</strong>
           </TextList.Item>
         </TextList>
       </>
